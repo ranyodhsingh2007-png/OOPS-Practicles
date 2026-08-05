9bc0a902b34ae66479794e7c3344319a7c9f81f2
ranyodhsingh2007-png <ranyodhsingh2007@gmail.com>
2026-08-05 16:00:17 +0530

    Add files via upload

---
+++ pract2whileloops.cpp
@@ -0,0 +1,35 @@
+//simple program to display student details using while loops
+#include <iostream>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int urn;
+    string clas;
+};
+int main(){
+    Student s[3];
+   
+    int i=0;
+    
+   do{
+       cout<<"Enter name  : ";
+       cin>>s[i].name;
+       cout<<"Enter URN   : ";
+       cin>>s[i].urn;
+       cout<<"Enter Class : ";
+       cin>>s[i].clas;
+       i++;}
+       while(i<=2);
+   i=0;
+   do{
+       cout<<"----STUDENT DETAIL----"<<endl;
+        cout<<"Name  :"<<s[i].name<<endl;
+        cout<<"URN   :"<<s[i].urn<<endl;
+        cout<<"Class :"<<s[i].clas<<endl;
+        cout<<"---------------------"<<endl;
+        i=i+1;
+   }while(i<=2);
+   
+   return 0;
+}
