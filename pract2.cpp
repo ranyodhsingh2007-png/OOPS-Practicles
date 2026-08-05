5be4a9e598322f5c954a64c21d642d38ac4a1b02
ranyodhsingh2007-png <ranyodhsingh2007@gmail.com>
2026-08-05 15:54:59 +0530

    Add files via upload

---
+++ pract2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int urn;
+    string clas;
+};
+int main(){
+    Student s;
+    s.name;
+    s.urn;
+    s.clas;
+    int n;
+    for(int i=1;i<=3;i++){
+        cout<<"Enter name: ";
+        cin>>s.name;
+        cout<<"Enter URN: ";
+        cin>>s.urn;
+        cout<<"Enter class: ";
+        cin>>s.clas;
+        
+    }
+    for(int i=1;i<=3;i++){
+        cout<<"Student Detail:-"<<endl;
+        cout<<"Name  :"<<s.name<<endl;
+        cout<<"URN   :"<<s.urn<<endl;
+        cout<<"Class :"<<s.clas<<endl;
+    }
+    return 0;
+}
