89e59af917b13ea8769390f2d07f484477042a30
ranyodhsingh2007-png <ranyodhsingh2007@gmail.com>
2026-08-05 15:37:17 +0530

    Add files via upload

---
+++ practical1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int roll;
+    string clas;
+};
+int main(){
+    Student s;
+    s.name;
+    s.roll;
+    s.clas;
+    cout<<"Enter name : "<<endl;
+    cin>>s.name;
+    cout<<"Enter URN  :"<<endl;
+    cin>>s.roll;
+    cout<<"Enter Class:"<<endl;
+    cin>>s.clas;
+    cout<<"Student Detail: "<<endl;
+    cout<<"Name  :"<<s.name<<endl;
+    cout<<"Class :"<<s.clas<<endl;
+    cout<<"URN   :"<<s.roll<<endl;
+    return 0;
+
+}
